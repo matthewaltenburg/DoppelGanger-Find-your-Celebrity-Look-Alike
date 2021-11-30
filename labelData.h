f343ebbefcacc6ef4bf8d4171edb04401eeacc99
Matthew Altenburg <mattaltenburg@gmail.com>
2021-11-30 16:16:29 +1100

    add new cpp images file

--- labelData.h
+++
@@ -1,7 +0,0 @@
-// label data
-// labelData.h
-#include <iostream>
-
-// Label -> Name Mapping file
-typedef std::map<string, string> Dict;
-Dict generateLabelMap(void);
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <Eigen/Dense>
 #include <dirent.h> // for opendir
 #include <dlib/clustering.h>
 #include <dlib/dnn.h>
@@ -8,15 +9,19 @@
 #include <dlib/opencv.h>
 #include <dlib/string.h>
 #include <filesystem> // for ittering through the filesystem
-#include <iostream>   // stdin and stdout
+#include <fstream>
+#include <iostream> // stdin and stdout
+#include <map>      // dictionaies
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <sstream>
 #include <string>
 #include <typeinfo>
 
 using namespace cv;
 using namespace std;
 using namespace dlib;
+using namespace Eigen;
 
 // This code defines a ResNet network.  It's basically copied
 // and pasted from the dnn_imagenet_ex.cpp example.
@@ -47,24 +52,21 @@ using anet_type = loss_metric<fc_no_bias<
     avg_pool_everything<alevel0<alevel1<alevel2<alevel3<alevel4<max_pool<
         3, 3, 2, 2,
         relu<affine<con<32, 7, 7, 2, 2, input_rgb_image_sized<150>>>>>>>>>>>>>;
-// ----------------------------------------------------------------------------------------
 
 // function for inroling the images
 void inrole_data(frontal_face_detector faceDetector,
                  shape_predictor shapePredictor, anet_type faceRecognizer);
 
-// ----------------------------------------------------------------------------------------
+void lookalike(frontal_face_detector faceDetector,
+               shape_predictor shapePredictor, anet_type faceRecognizer);
 
 // Main function
 int main() {
 
-  // testPoint
-  cout << "Starting..." << endl;
-
   // Set up the faceDetector
   frontal_face_detector faceDetector = get_frontal_face_detector();
   // Set up the shapePredictor
-  String face68 = "/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/"
+  string face68 = "/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/"
                   "shape_predictor_68_face_landmarks.dat";
   shape_predictor shapePredictor;
   deserialize(face68) >> shapePredictor;
@@ -79,16 +81,15 @@ int main() {
           "faceRecognizer!"
        << endl;
 
-  inrole_data(faceDetector, shapePredictor, faceRecognizer);
+  ifstream f("/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/"
+             "descriptors.csv");
 
-  // End ----------------------------------------------------------------
-
-  // testPoint
-  // cout << "number of face descriptors " << faceDescriptors.size() << endl;
-  // cout << "number of face labels " << faceLabels.size() << endl;
-
-  // testPoint
-  cout << "Worked to the end of the file" << endl;
+  if (f.good() == false) {
+    cout << "Creating faceDetector CSV file enrollment" << endl;
+    inrole_data(faceDetector, shapePredictor, faceRecognizer);
+  }
+  cout << "Running new image enrollment" << endl;
+  lookalike(faceDetector, shapePredictor, faceRecognizer);
 
   return 0;
 };
@@ -99,27 +100,133 @@ void inrole_data(frontal_face_detector faceDetector,
 //     in the images folder and names files and stores them as face descriptors
 //     and labels.
 {
-  // create a dictionary to uses for containing file label and person name
-  std::map<int, string> index;
-  // create a vector for storing face descriptors
-  std::vector<matrix<float, 0, 1>> faceDescriptors;
 
-  std::cout << "directory_iterator" << endl;
-
-  // Start --------------------------------------------------------------
   string celebFolder =
-      "/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/celeb_mini";
+      "/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/celeb_mini2";
+  int x = 0;
+  // for writing to disk
+  const string descriptorsPath =
+      "/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/"
+      "descriptors.csv";
+  ofstream output;
+  output.open(descriptorsPath);
+
   for (auto const &images : std::filesystem::directory_iterator{celebFolder})
     for (auto const &image : std::filesystem::directory_iterator{images}) {
-      // load the image using the Dlib library
-      matrix<rgb_pixel> img;
-      load_image(img, image.path());
+      cout << image.path().stem() << endl;
 
-      // run face detection
-      // Stoped Here ****************************************************
+      // load the image using the Dlib library
+      matrix<rgb_pixel> imDlib;
+      load_image(imDlib, image.path());
+
+      // run face detection to detect faces in image
+      std::vector<dlib::rectangle> faceRects = faceDetector(imDlib);
+
+      // loop through the faces in in image and find shape and descriptors
+      for (int j = 0; j < faceRects.size(); j++) {
+
+        // Find facial landmarks for each detected face
+        full_object_detection shape = shapePredictor(imDlib, faceRects[j]);
+
+        // Compute face descriptor using neural network defined in Dlib.
+        matrix<rgb_pixel> face_chip;
+        extract_image_chip(imDlib, get_face_chip_details(shape, 150, 0.25),
+                           face_chip);
+        matrix<float, 0, 1> faceDescriptor = faceRecognizer(face_chip);
+
+        output << image.path().stem();
+        for (auto const &face : faceDescriptor) {
+          output << "," << face;
+        };
+        output << endl;
+      }
+
+      x++;
+      cout << x << endl;
     };
 
-  // End ----------------------------------------------------------------
-
-  cout << "function worked" << endl;
+  output.close();
 }
+
+void lookalike(frontal_face_detector faceDetector,
+               shape_predictor shapePredictor, anet_type faceRecognizer) {
+  // Inrole any new face detections
+  string path;
+  cout << "please enter new image path" << endl;
+  getline(cin, path);
+
+  ifstream ff(path);
+
+  while (ff.good() == false) {
+    cout << "image not found please enter new image path" << endl;
+    getline(cin, path);
+    ifstream ff(path);
+    if (ff.good()) {
+      break;
+    }
+  }
+  cout << "image found" << endl;
+
+  matrix<rgb_pixel> imDlib;
+  load_image(imDlib, path);
+
+  std::vector<dlib::rectangle> faceRects = faceDetector(imDlib);
+  matrix<float, 0, 1> faceDescriptor;
+  // Compute face descriptor using neural network defined in Dlib.
+  for (int j = 0; j < faceRects.size(); j++) {
+
+    // Find facial landmarks for each detected face
+    full_object_detection shape = shapePredictor(imDlib, faceRects[j]);
+
+    // Compute face descriptor using neural network defined in Dlib.
+    matrix<rgb_pixel> face_chip;
+    extract_image_chip(imDlib, get_face_chip_details(shape, 150, 0.25),
+                       face_chip);
+    faceDescriptor = faceRecognizer(face_chip);
+  };
+
+  // import csv to map object with the names of the celeb's and the 128 vectors
+  std::map<std::string, dlib::matrix<float, 0, 1>> cele_dict;
+  dlib::array<float> float_array = dlib::array<float>();
+
+  ifstream csvFile;
+  csvFile.open("/home/matthew/DoppelGanger-Find-your-Celebrity-Look-Alike/"
+               "descriptors.csv");
+  string name;
+
+  while (csvFile.good()) {
+    getline(csvFile, name);
+    stringstream ss(name);
+    std::string substr;
+    getline(ss, substr, ',');
+    string fileName = substr;
+    float_array.clear();
+
+    while (ss.good()) {
+      getline(ss, substr, ',');
+      float_array.push_back(stof(substr));
+    }
+    auto float_mat = mat(float_array);
+    auto float_matrix = matrix<float, 0, 1>(float_mat);
+
+    cele_dict[fileName] = float_matrix;
+  }
+
+  double minDistance = 1;
+  string label;
+
+  // compairs the arrays
+  for (auto i : cele_dict) {
+    if (i.second.size() > 0) {
+      double distance = dlib::length(faceDescriptor - i.second);
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        label = i.first;
+      };
+    };
+  }
+  csvFile.close();
+
+  cout << "Picture looks like " << label << endl;
+};
